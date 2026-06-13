4387ee78b86f75fe0d8640c2b7e542714ca55783
Felipe Jeldes Ramirez <felipejeldesramirez@MacBook-Pro-de-Felipe.local>
2026-06-13 19:32:38 -0400

    fix: wrap CREATE VIEW in DO block to survive fresh DBs, remove COMMIT from ALTER TYPE migration

--- migrations/009a_alter_eval_type_enum.sql
+++ migrations/009a_alter_eval_type_enum.sql
@@ -1,9 +1,8 @@
 -- Migration: Add new values to eval_type_enum
--- Must run outside a transaction because ALTER TYPE ... ADD VALUE
--- cannot be used in a transaction that also uses the new value.
+-- Separated from 009 to avoid UnsafeNewEnumValueUsage:
+-- ALTER TYPE ADD VALUE cannot be in the same transaction
+-- as statements that use the new value.
 -- Date: 2026-06-11
-
-COMMIT;
 
 ALTER TYPE eval_type_enum ADD VALUE IF NOT EXISTS 'free';
 ALTER TYPE eval_type_enum ADD VALUE IF NOT EXISTS 'pro_monthly';
